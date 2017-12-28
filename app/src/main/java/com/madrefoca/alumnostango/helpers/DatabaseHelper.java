bd0a15d84a716f7ad31a724c4361d7308d8a2780
Fernando <gdp.fernando@gmail.com>
2017-12-28 18:32:58 -0300

    changed db version and changed gradle stuff

--- app/src/main/java/com/madrefoca/alumnostango/helpers/DatabaseHelper.java
+++ app/src/main/java/com/madrefoca/alumnostango/helpers/DatabaseHelper.java
@@ -29,7 +29,7 @@ import java.sql.SQLException;
 public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
 
     private static final String DATABASE_NAME = "AlumnosTango.db";
-    private static final int DATABASE_VERSION = 2;
+    private static final int DATABASE_VERSION = 6;
 
     private Dao<Attendee, Integer> attendeeDao;
     private Dao<AttendeeEventPayment, Integer> attendeeEventPaymentDao;
